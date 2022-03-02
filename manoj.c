4ac6776a1b594f09b8fa3c56c8cef5de8414f10a
manojhunasimarad <89728596+manojhunasimarad@users.noreply.github.com>
2022-03-02 12:33:05 +0530

    Update manoj.c

--- manoj.c
+++ manoj.c
@@ -6,8 +6,8 @@
 #define SIZE 1024
 
 
-
-void send_file(FILE *fp, int sockfd){
+void send_file(FILE *fp, int sockfd)
+{
   int n;
   char data[SIZE] = {0};
  
@@ -20,14 +20,18 @@ void send_file(FILE *fp, int sockfd){
     bzero(data, SIZE);
   }
 }
- 
+
+
+
+
+
  void write_file(int sockfd){
   int n;
   FILE *fp;
   // char *filename = "recv.txt";
 
 char receiver[500];
-   printf("enter the file name along with extension to be received");
+   printf("enter the file name along with extension to be received\n");
  scanf("%s",receiver);
   char *filename = receiver;
   char buffer[SIZE];
@@ -44,19 +48,77 @@ char receiver[500];
   }
   return;
 }
+
+
+int client()
+{
+    //   char *ip = "192.168.60.41";
+char sip[200];
+  printf("Enter ip address of the client which is sending file system {other system }\n");
+ scanf("%s",sip);
  
-int main()
+ char *ip = sip;
+
+  int port = 8080;
+  int e,flag;
+ 
+  int sockfd;
+  struct sockaddr_in server_addr;
+  FILE *fp;
+ char stringer[500];
+ printf("enter the file name along with extension to be sent\n");
+ scanf("%s",stringer);
+  char *filename = stringer;
+  sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  if(sockfd < 0) {
+    perror("[-]Error in socket");
+    exit(1);
+  }
+  printf("[+]Server socket created successfully.\n");
+ 
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_port = port;
+  server_addr.sin_addr.s_addr = inet_addr(ip);
+//  printf("The connection has been established succesfully between client and server\n");
+  e = connect(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr));
+  if(e == -1) {
+    perror("[-]Error in socket");
+    exit(1);
+  }
+ printf("[+]Connected to Server.\n");
+ 
+  fp = fopen(filename, "r");
+  if (fp == NULL) {
+    perror("[-]Error in reading file.\n");
+    exit(1);
+  }
+ 
+  send_file(fp, sockfd);
+  printf("[+]File data sent successfully.\n");
+ 
+  printf("[+]Closing the connection.\n");
+  if(flag==2){printf("The connection status is : communication done successfully\n");}
+else if(flag==1) {printf("The connection status is : connected yet to communicate\n");}
+else{printf("The connection status is : not connected\n");}
+  close(sockfd);
+
+ 
+
+  return 0;
+}
+
+
+
+int server()
 {
-     
-  char input;
-  printf("pleaser input server as s or client as c\n");
-  scanf("%c",&input);
-
-
-
-if(input=='s')
+    
 {
-    char *ip = "192.168.60.79";
+
+    // char *ip = "192.168.60.79";
+    char rip[200];
+    printf("enter  ip address of the server which is reciving {your system}\n");
+    scanf("%s",rip);
+     char *ip = rip;
      int port = 8080;
      int e;
      int sockfd, new_sock;
@@ -70,87 +132,85 @@ if(input=='s')
     
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if(sockfd < 0) {
-    perror("[-]Error in socket");
+    perror("[-]Error in socket\n");
     exit(1);
-    }
-    printf("[+]Server socket created successfully.\n");
 
+    }
+    //  printf("The connection has been established succesfully between client and server");
+    printf("[+]Server socket created successfully.\n");
+   
+
+
+   
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = port;
     server_addr.sin_addr.s_addr = inet_addr(ip);
     
     e = bind(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr));
     if(e < 0) {
-    perror("[-]Error in bind");
+    perror("[-]Error in bind\n");
     exit(1);
   }
   printf("[+]Binding successfull.\n");
-
+ int flag=1; //connection is done
     
      
   if(listen(sockfd, 10) == 0){
  printf("[+]Listening....\n");
+ flag=3; //connected but ready to communication
  }else
  {
- perror("[-]Error in listening");
+ perror("[-]Error in listening\n");
     exit(1);
  }
  
   addr_size = sizeof(new_addr);
   new_sock = accept(sockfd, (struct sockaddr*)&new_addr, &addr_size);
   write_file(new_sock);
+  flag=2; //communication started and finished
   printf("[+]Data written in the file successfully.\n");
+  
+
+
+
+
+  if(flag==2){printf("The connection status is : communication done successfully\n");}
+else if(flag==1) {printf("The connection status is : connected yet to communicate\n");}
+else{printf("The connection status is : not connected\n");}
  return 0;
 }
 
-else if(input=='c')
-{
- 
- 
-  char *ip = "192.168.60.39";
-  int port = 8080;
-  int e;
- 
-  int sockfd;
-  struct sockaddr_in server_addr;
-  FILE *fp;
- char stringer[500];
- printf("enter the file name along with extension to be sent");
- scanf("%s",stringer);
-  char *filename = stringer;
-  sockfd = socket(AF_INET, SOCK_STREAM, 0);
-  if(sockfd < 0) {
-    perror("[-]Error in socket");
-    exit(1);
-  }
-  printf("[+]Server socket created successfully.\n");
- 
-  server_addr.sin_family = AF_INET;
-  server_addr.sin_port = port;
-  server_addr.sin_addr.s_addr = inet_addr(ip);
- 
-  e = connect(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr));
-  if(e == -1) {
-    perror("[-]Error in socket");
-    exit(1);
-  }
- printf("[+]Connected to Server.\n");
- 
-  fp = fopen(filename, "r");
-  if (fp == NULL) {
-    perror("[-]Error in reading file.");
-    exit(1);
-  }
- 
-  send_file(fp, sockfd);
-  printf("[+]File data sent successfully.\n");
- 
-  printf("[+]Closing the connection.\n");
-  close(sockfd);
- 
-  return 0;
+
+
+
+
 }
 
 
 
+int main()
+{
+
+    int flag=0;
+    char choice;
+
+printf("Do you want to establish connection(y/n)?\n");
+
+scanf(" %c",&choice);
+if(flag==2){printf("communication done successfully\n");}
+else if(flag==1) {printf("connected yet to communicate\n");}
+else{printf("not connected\n");}
+if(choice=='n'){return 0;}
+
+
+ char input;
+  printf("pleaser input server as s or client as c\n");
+  scanf(" %c",&input);
+
+if(input=='s')
+{server();}
+else if(input=='c')
+{client();}
+
+ return 0; 
 }
